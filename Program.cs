4847fa1a60678aa4787fd08c637627a35097639b
snehau259 <snehau259@gmail.com>
2023-03-28 00:59:08 +0530

    taking input from console and string writeline

--- Program.cs
+++ Program.cs
@@ -7,8 +7,15 @@ namespace firstDNProject
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("enter your name:");
+            String name=Console.ReadLine();
+                        Console.WriteLine("hello "+name);//concatenation
+                        Console.WriteLine("hello {0}",name);//COMPOSITE FORMATTING
+                        Console.WriteLine($"hello {name}");//string interpolation
+
+                        Console.ReadKey();
+
         }
-    }
+    } ̰
 }
 
