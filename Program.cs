f3e633c3a845114b8276d49c7cba8e473e7e6491
snehau259 <snehau259@gmail.com>
2023-03-28 00:39:47 +0530

    print hello

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace firstDNProject
+{
+    class myClass
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
+
