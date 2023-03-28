bf68dc10c3e9e8df1f2c9d9059f93875820c8fec
snehau259 <snehau259@gmail.com>
2023-03-28 12:00:08 +0530

    class,objects,constructors

--- Program.cs
+++ Program.cs
@@ -5,19 +5,11 @@ namespace firstDNProject
 {
     class myClass
     {//methods
-        static void MyMethod()
-        {
-            Console.WriteLine("I just got executed!");
-        }
-        static int greatest(int num1, int num2)
-        {
-            return num1 > num2 ? num1 : num2;
-        }
+       
         public static void Main(String[] args)
         {
-            MyMethod();
-            int big = greatest(40, 45);
-            Console.WriteLine(big);
+           Fruit apple=new Fruit("red");
+           apple.displayColor();
 
         }
     }
---
+++ fruit.cs
@@ -0,0 +1,14 @@
+public class Fruit
+{
+public string color;
+
+    public Fruit(string color)
+    {
+        this.color = color;
+    }
+
+    public void displayColor()
+{
+    Console.WriteLine(color);
+}
+}
